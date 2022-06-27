9634ef744b9adbaa68f8798cfa754de611ce8275
unknown <lau1087054@gmail.com>
2022-06-27 00:04:45 -0700

    edits

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -2,19 +2,19 @@ const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
     test("Getting the Correct Name", () => {
-        var example1= new Employee("Eric", 1211, "Lau1087054@gmail.com");
-         expect(example1.getName()) .toBe("Eric")
+        var example1= new Employee("John", 1211, "Asdfqwerty@gmail.com");
+         expect(example1.getName()) .toBe("John")
     })
     test("Getting the Correct Id", () => {
-        var example1= new Employee("Eric", 1211, "Lau1087054@gmail.com");
+        var example1= new Employee("John", 1211, "Asdfqwerty@gmail.com");
          expect(example1.getId()) .toBe(1211)
     })
     test("Getting the Correct Email", () => {
-        var example1= new Employee("Eric", 1211, "Lau1087054@gmail.com");
-         expect(example1.getEmail()) .toBe("Lau1087054@gmail.com")
+        var example1= new Employee("John", 1211, "Asdfqwerty@gmail.com");
+         expect(example1.getEmail()) .toBe("Asdfqwerty@gmail.com")
     })
     test("Getting the Correct Email", () => {
-        var example1= new Employee("Eric", 1211, "Lau1087054@gmail.com");
+        var example1= new Employee("John", 1211, "Asdfqwerty@gmail.com");
          expect(example1.getRole()) .toBe("Employee")
 } ) 
 })
---
+++ test/Manager.test.js
@@ -0,0 +1,20 @@
+const Engineer = require("../lib/Engineer");
+
+describe("Employee", () => {
+    test("Getting the Correct Name", () => {
+        var example1= new Employee("Eric", 1211, "Lau1087054@gmail.com");
+         expect(example1.getName()) .toBe("Eric")
+    })
+    test("Getting the Correct Id", () => {
+        var example1= new Employee("Eric", 1211, "Lau1087054@gmail.com");
+         expect(example1.getId()) .toBe(1211)
+    })
+    test("Getting the Correct Email", () => {
+        var example1= new Employee("Eric", 1211, "Lau1087054@gmail.com");
+         expect(example1.getEmail()) .toBe("Lau1087054@gmail.com")
+    })
+    test("Getting the Correct Email", () => {
+        var example1= new Employee("Eric", 1211, "Lau1087054@gmail.com");
+         expect(example1.getRole()) .toBe("Employee")
+} ) 
+})
